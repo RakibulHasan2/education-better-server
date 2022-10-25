db521ba919648c06caa90dc37410069cebe637c7
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-10-25 13:04:16 +0600

    fake data created

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
-const app = express();
-const part = process.env.PORT || 5000;
+const courses = require( './Data/Courses.json')
 
-app.use(cors());
+app.use(cors())
 
-app.get( '/',(req, res) => {
-    res.send("running");
-});
+app.get( '/courses', (req, res) => {
+    res.send(courses)
+})
+app.get( '/', (req, res) => {
+    res.send('the education better server is running')
+})
 
-app.listen(port, () =>{
-    console.log(`running on port, ${port}`)
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id 
+    const course = courses.find(cr =>cr.id === id)
+    res.send(course)
+})
+
+app.listen(port , () =>{
+    console.log(`port is running, ${port}`)
 })
